4abcf9e662b0dd6df9ec2ae529fd6b4206bced94
olivia <trevorleecox@icloud.com>
2025-08-19 14:50:55 -0500

    basicly forget to add the src atubertu so it was empty

--- src/Componet/UIAudio.hpp
+++ src/Componet/UIAudio.hpp
@@ -7,6 +7,7 @@ public:
   UIAudio(const std::string& URL, const bool controls = true)
     : Component("audio"),controls(controls) {
         setAttr("class", "ui-audio ui-audio-" + URL);
+	setAttr("src", URL);
 	if(controls){
 	  setAttr("controls", "");
 	}
